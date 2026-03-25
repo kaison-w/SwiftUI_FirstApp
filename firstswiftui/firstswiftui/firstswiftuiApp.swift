d2b4349581f5330b5f1680fbe359ade8e214e83d
kaison-w <dwayneward2004@gmail.com>
2026-03-25 12:49:18 -0500

    Add files via upload

---
+++ firstswiftui/firstswiftui/firstswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstswiftuiApp.swift
+//  firstswiftui
+//
+//  Created by Ward, Kaison on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct firstswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
